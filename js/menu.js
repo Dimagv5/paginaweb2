c9d609b21968e784e2d75cf5f75154f1587a55f6
Dimag <dianagv@outlook.com>
2018-10-29 16:57:39 -0500

    version 1

---
+++ js/menu.js
@@ -0,0 +1,6 @@
+let btnmenu = document.getElementById('btnmenu');
+let menu = document.getElementById('menu');
+
+btnmenu.addEventListener('click', function(){
+   menu.classList.toggle('mostrar'); 
+});
